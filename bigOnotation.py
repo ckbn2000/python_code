39643485fb0c4d72db011746b72aa63c510b768f
CHANDRAKANTH <ckbn2000@gmail.com>
2019-01-19 17:19:47 -0800

    big o file

--- bigOnotation.py
+++ bigOnotation.py
@@ -1,1 +1,19 @@
-3W5
+def print_first_item(items):
+    print(items[0])
+
+def print_all_items(items):
+    for item in items:
+        print(item)
+
+def print_all_possible_ordered_pairs(items):
+    for first_item in items:
+        print(items)
+        for second_item in items:
+            print(second_item)
+            print (first_item, second_item)
+items = [1,2,3,4]
+print_first_item(items)
+print_all_items(items)
+print_all_possible_ordered_pairs(items)
+
+
